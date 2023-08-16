47d4018785f69b92db5dc499f6cb078559431505
Harry Patiry <60627616+harrypatiry@users.noreply.github.com>
2023-08-16 17:23:00 -0400

    server

--- server/index.js
+++ server/index.js
@@ -1,55 +1,44 @@
-const path = require('path')
 const express = require('express')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const webpack = require('webpack')
-const middleware = require('webpack-dev-middleware')
-const webpackConfig = require('../webpack.config')
-const compiler = webpack(webpackConfig)
-
+const path = require('path')
+const cors = require('cors')
 const app = express()
-const PORT = process.env.PORT || 5940
+const pool = require('./database')
+const port = 5000
 
-app.listen(PORT, () => console.log(`Server Listening on ${PORT}`))
+// logging middleware
+app.use(cors())
+console.info('Starting server')
 
-app.use(require("webpack-hot-middleware")(compiler, {
-  'log': false, 
-  'path': '/__webpack_hmr', 
-  'heartbeat': 10 * 1000
-}))
+// body parsing middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.use(middleware(compiler, {
-  noInfo: true,
-  publicPath: webpackConfig.output.publicPath
-}))
-
-app.use(morgan('dev'))
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-
-app.use(express.static(path.join(__dirname, '..', 'src')))
+// static middleware
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'))
-})
+}) // Send index.html for any other requests
 
-app.use((req, res, next) => {
-  if (path.extname(req.path).length > 0) {
-    res.status(404).end()
-  } else {
-    next()
-  }
-})
-
-app.get('/*', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '..', 'src', 'index.html'))
-})
-
+// error handling middleware
 app.use((err, req, res, next) => {
-  console.error(err, typeof next)
   console.error(err.stack)
-  res.status(err.status || 500).send(err.message || 'Internal server error.')
+  res.status(err.status || 500).send(err.message || 'Internal server error')
+})
+
+app.listen(port, () => {
+  console.log('listening on port ' + port)
+})
+
+//routes
+
+//create user
+app.post('/users', async(req, res) => {
+  try {
+    console.log(req.body)
+  } catch(err) {
+    console.error(err.message)
+  }
 })
 
 
