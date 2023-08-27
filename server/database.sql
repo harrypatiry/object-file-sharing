834eca23ab7e3758ada7fbf950472cb35875fcdc
Harry Patiry <60627616+harrypatiry@users.noreply.github.com>
2023-08-27 16:01:57 -0400

    Revert "Revert "post crud""
    This reverts commit 130a56de458796fb3f359e7942cca4583fbd66d3.

---
+++ server/database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE ofs;
+
+CREATE TABLE users (
+    user_id serial primary key,
+    email varchar(255) unique not null,
+    username varchar(255) unique not null,
+    password varchar(255) not null,
+    created_at date default current_date
+);
---
+++ server/validators/auth.js
@@ -0,0 +1,45 @@
+const { check } = require('express-validator');
+const db = require('../database/db');
+const { compare } = require('bcryptjs');
+
+//password 
+const username = check('username').isLength({ min: 2, max: 15})
+.withMessage('Username must be between 2 and 15 characters.')
+
+//password 
+const password = check('password').isLength({ min: 6, max: 15})
+.withMessage('Password must be between 6 and 15 characters.')
+
+//email 
+const email = check('email').isEmail()
+.withMessage('Please enter a valid email address.');
+
+//check if email exists
+const emailExists = check('email').custom(async (value) => {
+    const { rows } = await db.query('SELECT * from users WHERE email = $1', [
+        value,
+    ])
+    if (rows.length) {
+        throw new Error('Email already exists.')
+    }
+})
+
+//login validation
+const loginFieldsCheck = check('email').custom(async (value, { req }) => {
+    const user = await db.query('SELECT * from users WHERE email = $1', [value])
+    if (!user.rows.length) {
+        throw new Error('Email does not exists.')
+    }
+
+    const validPassword = await compare(req.body.password, user.rows[0].password)
+    if (!validPassword) {
+        throw new Error('Wrong password')
+    }
+    
+    req.user = user.rows[0]
+})
+
+module.exports = {
+    registerValidation: [username, email, password, emailExists],
+    loginValidation: [loginFieldsCheck]
+}
