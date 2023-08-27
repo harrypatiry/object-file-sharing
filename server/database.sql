6995ff1dbdf549a72ed116585a7e60b369a0baf8
Harry Patiry <60627616+harrypatiry@users.noreply.github.com>
2023-08-27 16:02:11 -0400

    Revert "Revert "fix""
    This reverts commit 16b247f964cf755f4b7bf5035025a1961438fd23.

--- server/database.sql
+++ server/database.sql
@@ -1,9 +1,22 @@
 CREATE DATABASE ofs;
 
 CREATE TABLE users (
-    user_id serial primary key,
+    id serial PRIMARY KEY,
     email varchar(255) unique not null,
     username varchar(255) unique not null,
     password varchar(255) not null,
     created_at date default current_date
 );
+
+-- store files in zip file
+
+CREATE TABLE posts (
+    id serial PRIMARY KEY,
+    author int FOREIGN KEY,
+    description VARCHAR(255),
+    cover varchar(255),
+    media text[] 
+)
+
+
+-- FROM posts INNER JOIN users ON posts.author = users.id;
